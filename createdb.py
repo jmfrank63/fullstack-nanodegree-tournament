6a0b68b059029eff327397fbc104ae557e000945
Johannes Maria Frank <jmfrank63@gmail.com>
2015-04-26 10:31:09 +0100

    added non working create database instruction

--- createdb.py
+++ createdb.py
@@ -3,9 +3,29 @@ import os
 
 print os.getcwd()
 
-with open('tournament.sql') as sql_file:
-    for line in sql_file.readlines:
-        print line
-
+def connect_db(db_name):
+    """Connects to a database and creates that database if it doesn't exist"""
+    try:
+        conn = psycopg2.connect('dbname=' + db_name)
+    except:
+        conn = psycopg2.connect('dbname=postgres')
+        cursor = conn.cursor()
+        cursor.execute('CREATE DATABASE ' + db_name)
+        cursor.close()
+        conn.close()
+        try:
+            conn = psycopg2.connect('dbname=' + db_name)
+        except Exception, e:
+            raise e
+    return conn
 
     
+# open the sql commands file
+if __name__ == '__main__':
+    conn = connect_db('tournament')
+    with open('tournament.sql') as sql_file:
+    # iterate over the lines
+        for line in sql_file:
+            if line.strip() and not line[:2] == '--':
+                print(line[:-2])
+
